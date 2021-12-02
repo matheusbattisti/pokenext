a715a774885e58f9e25a49ee0758136048bc8772
Matheus Battisti <matheusbudkewicz@gmail.com>
2021-12-02 21:04:07 -0200

    correcoes projeto

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,7 +1,7 @@
 import Link from 'next/link'
 import Image from 'next/image'
 
-import styles from '../../styles/Navbar.module.css'
+import styles from '../styles/Navbar.module.css'
 
 export default function Navbar() {
   return (
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import '../styles/globals.css'
 
-import Layout from './components/Layout'
+import Layout from '../components/Layout'
 
 function MyApp({ Component, pageProps }) {
   return (
